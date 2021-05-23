8627fecd9a6c253cfd89146575676209f3ca94ef
Can Artuc <canartuc@gmail.com>
2021-05-23 22:21:47 +0200

    Database controller has been added

---
+++ contDB.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+func Write2Json(filename string) {
+	allPosts, err := GetAllPosts()
+	if err != nil {
+		log.Fatal("(ERR) Couldn't get all posts: ", err)
+	}
+
+	jsonBeauty, err := json.MarshalIndent(allPosts, "", "   ")
+	if err != nil {
+		log.Fatal("(ERR) Couldn't beautify the JSON serialization: ", err)
+	}
+
+	err = ioutil.WriteFile(filename, jsonBeauty, 0777)
+	if err != nil {
+		log.Fatalf("(ERR) Couldn't write beautify JSON to the file: %s : %x", filename, err)
+	}
+}
